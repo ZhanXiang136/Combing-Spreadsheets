e3e7ab223683d64aefe6f227d902991ba7a70da3
Zhan Xiang Zheng <zhanxiangzhen136@gmail.com>
2023-04-02 20:32:19 -0400

    Combined all files together

--- main.py
+++ main.py
@@ -22,74 +22,108 @@ def determine_correct_pandas_conversion(file):
         print(f'\nError: Invalid extension {extension}')
 
 
-def create_column_name(file_to_be_added):
-    return 'event'
-    column_list = list(file_to_be_added.columns.values)
+def create_column_name(main_file):
+    column_list = list(main_file.columns.values)
     while True:
         new_name = input('\nWhat do you want to name your new column?: ')
         if new_name not in column_list:
             return new_name
 
-
 def main():
-    invalid_osis_list = []
-    #print("Please make sure that the file you're going to add have the following column: First Name, Last Name, Osis, Points")
-    #file_to_be_added = check_file_existence('\nEnter the file to be added: ')
-    #file_to_add = check_file_existence('\nEnter the file to add: ')
+    print("Welcome to the Program")
+    #look for spreadsheet
+    main_file = 'main.xlsx'
+    if not exists('main.xlsx'):
+        main_file = check_file_existence('\nEnter the main spreadsheet: ')
+    event_file = 'event.xlsx'
+    if not exists('event.xlsx'):
+        event_file = check_file_existence('\nEnter the event spreadsheet: ')
 
-    df_to_be_added = pd.read_excel('main.xlsx', sheet_name=0)#determine_correct_pandas_conversion(file_to_be_added)
-    df_to_add = pd.read_excel('event.xlsx', sheet_name=0)#determine_correct_pandas_conversion(file_to_add)
+    #open up the spreadsheet
+    df_main = determine_correct_pandas_conversion(main_file)
+    df_event = determine_correct_pandas_conversion(event_file)
 
-    new_column_name = create_column_name(df_to_be_added)
-    df_to_be_added[new_column_name] = None
+    invalid_list = []
 
-    if all(item in list(df_to_add.columns.values) for item in ['First Name', 'Last Name', 'Osis', 'Points']) and all(
-            item in list(df_to_be_added.columns.values) for item in
-            ['First Name', 'Last Name', 'Osis', 'Total Points']):
-        index_of_total_point = list(df_to_be_added.columns.values).index('Total Points')
-        for index, row in df_to_add.iterrows():
-            osis_add = row['Osis']
-            points_add = row['Points']
-            first_name_add = row['First Name']
-            last_name_add = row['Last Name']
+    needed_column_names = ['First Name', 'Last Name']
 
-            found_osis = df_to_be_added.loc[df_to_be_added['Osis'] == osis_add]
-            length_of_osis_found = len(found_osis)
+    cal_what = input("(P)oints or (H)ours or (B)oth: ")
+    if cal_what.lower().strip() == 'points' or cal_what.lower().strip() == 'p':
+        look_for_points = True
+        look_for_hours = False
+        needed_column_names.append('Points')
+    elif cal_what.lower().strip() == 'hours' or cal_what.lower().strip() == 'h':
+        look_for_points = False
+        look_for_hours = True
+        needed_column_names.append('Hours')
+    else:
+        look_for_points = True
+        look_for_hours = True
+        needed_column_names.append('Hours')
+        needed_column_names.append('Points')
 
-            if length_of_osis_found == 1:
-                row_info_added = df_to_be_added.iloc[found_osis.index[0]]
+    pattern = input("Sort by what? (E)mail, (O)sis?: ")
+    if pattern.lower().strip() == 'osis' or pattern.lower().strip() == 'o':
+        pattern = 'Osis'
+        needed_column_names.append('Osis')
+    elif pattern.lower().strip() == 'email' or pattern.lower().strip() == 'e':
+        pattern = 'Email'
+        needed_column_names.append('Email')
+
+
+    if all(item in list(df_event.columns.values) for item in needed_column_names) and all(
+            item in list(df_main.columns.values) for item in needed_column_names):
+        for index, row in df_event.iterrows():
+            pattern_add = row[pattern]
+            points_add = row['Points'] if look_for_points else None
+            hours_add = row['Hours'] if look_for_hours else None
+            first_name_add = row['First Name'].capitalize()
+            last_name_add = row['Last Name'].capitalize()
+
+            found_row = df_main.loc[df_main[pattern] == pattern_add]
+            len_of_found_rows = len(found_row)
+
+            if len_of_found_rows >= 1:
+                row_info_added = df_main.iloc[found_row.index[0]]
                 first_name_added = row_info_added['First Name']
                 last_name_added = row_info_added['Last Name']
-                total_point = row_info_added['Total Points'] if row_info_added['Total Points'] is None else 0
                 if first_name_added.lower().strip() == first_name_add.lower().strip() and last_name_added.lower().strip() == last_name_add.lower().strip():
-                    df_to_be_added.loc[df_to_be_added['Osis'] == osis_add, [new_column_name]] = [points_add]
+                    if look_for_points:
+                        df_main.loc[df_main[pattern] == pattern_add, ['Points']] = [points_add]
+                    if look_for_hours:
+                        df_main.loc[df_main[pattern] == pattern_add, ['Hours']] = [hours_add]
                 else:
-                    print(f'\nOsis {osis_add} has name {first_name_add} {last_name_add} in the event spreadsheet and {first_name_added} {last_name_added} in the main spreadsheet')
+                    print(f'\n{pattern} {pattern_add} has name {first_name_add} {last_name_add} in the event spreadsheet and {first_name_added} {last_name_added} in the main spreadsheet')
                     user_input = input("Press enter to add and continue, press x to skip and continue")
                     if user_input == 'x':
-                        invalid_osis_list.append(osis_add)
+                        invalid_list.append(pattern_add)
                     else:
-                        df_to_be_added.loc[df_to_be_added['Osis'] == osis_add, [new_column_name]] = [points_add]
+                        if look_for_points:
+                            df_main.loc[df_main[pattern] == pattern_add, ['Points']] = [points_add]
+                        if look_for_hours:
+                            df_main.loc[df_main[pattern] == pattern_add, ['Hours']] = [hours_add]
             else:
-                if length_of_osis_found == 0:
-                    print(f'\nOSIS with {osis_add} not found in the main database')
+                if len_of_found_rows == 0:
+                    print(f'\n{pattern} with {pattern_add} not found in the main database')
                 else:
-                    print(f'\nFound multiple OSIS with {osis_add} in the main database')
-                invalid_osis_list.append(osis_add)
+                    print(f'\nFound multiple {pattern} with {pattern_add} in the main database')
+                invalid_list.append(pattern_add)
 
-        print('\nThe following osis in the main spreadsheet are not added:')
-        for i in invalid_osis_list:
+        print(f'\nThe following {pattern} in the main spreadsheet are not added:')
+        for i in invalid_list:
             print(i)
 
-        df_to_be_added['Total Points'] = df_to_be_added.iloc[:, index_of_total_point+1:].sum(axis=1)
-        df_to_be_added.to_excel('new.xlsx', index=False)
+        df_main.to_excel('new.xlsx', index=False)
 
         print('\nProgram ended, thank you for using this code')
 
     else:
         print('\nYou are missing one or more the of the required column name')
+        print(f'\nMain spreadsheet contains {list(df_main.columns.values)} and needed {needed_column_names}')
+        print(f'\nEvent spreadsheet contains {list(df_event.columns.values)} and needed {needed_column_names}')
 
 
 if __name__ == '__main__':
     main()
-    print("Done")
+    print("Program Ended Done")
+
